3fa92c2be6819c9ef763f37d5527d16444b9b504
Hayley Tong <hayley.tong@sainsburys.co.uk>
2021-10-11 17:04:04 +0100

    changed backgound colour from white to black

--- src/components/CustomSelect.vue
+++ src/components/CustomSelect.vue
@@ -1,111 +1,120 @@
 <template>
-   <div class="custom-select" >
+  <div class="custom-select">
+    <div
+      class="selected"
+      tabindex="0"
+      :class="{ open: open }"
+      @click="toggleDropdown"
+      @keyup.enter="toggleDropdown"
+    >
+      <img :src="currency.flag" />
+      <span>{{ currency.code }}</span>
+    </div>
 
-      <div class="selected" tabindex="0" :class="{ open: open }" @click="toggleDropdown" @keyup.enter="toggleDropdown">
-        <img :src="currency.flag" />
-        <span>{{currency.code}}</span>
-      </div>
+    <div class="dropdown" :class="{ selectHide: !open }">
+      <input
+        type="text"
+        tabindex="0"
+        class="search"
+        v-model="search"
+        placeholder="search..."
+      />
 
-      <div class="dropdown" :class="{selectHide : !open}" >
-         <input type="text" tabindex="0" class="search" v-model="search" placeholder="search..." />
-         
-         <div class="items">
-            <div 
-               tabindex="0"
-               class="option"
-               v-for="currency in filteredCurrencies" 
-               :key="currency.code"
-               @keyup.enter="
-                  selected = currency;
-                  open = false;
-                  $emit('currency-selected', currency);         
-               "      
-               @click="
-                  selected = currency;
-                  open = false;
-                  $emit('currency-selected', currency);
-               ">
-                  <img :src="currency.flag" />
-                  <span>{{currency.code}}</span>
-            </div>
-         </div>
-         
+      <div class="items">
+        <div
+          tabindex="0"
+          class="option"
+          v-for="currency in filteredCurrencies"
+          :key="currency.code"
+          @keyup.enter="
+            selected = currency;
+            open = false;
+            $emit('currency-selected', currency);
+          "
+          @click="
+            selected = currency;
+            open = false;
+            $emit('currency-selected', currency);
+          "
+        >
+          <img :src="currency.flag" />
+          <span>{{ currency.code }}</span>
+        </div>
       </div>
-   </div>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
-   props: {
-      currencies: {
-         type: Array,
-         required: true,
-      },
-      currency: {
-         type: Object,
-         required: true,
+  props: {
+    currencies: {
+      type: Array,
+      required: true,
+    },
+    currency: {
+      type: Object,
+      required: true,
+    },
+  },
+  data() {
+    return {
+      open: false,
+      search: "",
+    };
+  },
+  computed: {
+    filteredCurrencies: function () {
+      var self = this;
+      return this.currencies.filter((currency) => {
+        return currency.search.indexOf(self.search.toUpperCase()) >= 0;
+      });
+    },
+  },
+  methods: {
+    toggleDropdown() {
+      this.open = !this.open;
+      this.search = "";
+    },
+    close(e) {
+      if (!this.$el.contains(e.target)) {
+        this.open = false;
       }
-   },
-   data() {
-      return {
-         open: false,
-         search: ""
-      };
-   },
-   computed: {
-      filteredCurrencies: function()
-      {
-         var self = this;
-         return this.currencies.filter((currency) => {
-            return (
-               currency.search.indexOf(self.search.toUpperCase()) >= 0
-            )
-         });
-      }
-   },
-   methods: {
-      toggleDropdown () {
-         this.open = !this.open
-         this.search = ""
-      },
-      close (e) {
-         if (!this.$el.contains(e.target)) {
-         this.open = false
-         }
-      }
-   },
-   mounted () {
-      document.addEventListener('click', this.close)
-   },
-   beforeUnmount () {
-      document.removeEventListener('click',this.close)
-   }
+    },
+  },
+  mounted() {
+    document.addEventListener("click", this.close);
+  },
+  beforeUnmount() {
+    document.removeEventListener("click", this.close);
+  },
 };
 </script>
 
 <style scoped>
-.selected, .option {
-   display: flex;
-   flex-direction: row;
+.selected,
+.option {
+  display: flex;
+  flex-direction: row;
 }
 
-img{
-   margin: 20px 0;
-   height: 20px;
-   border: 1px #ccc solid;
+img {
+  margin: 20px 0;
+  height: 20px;
+  border: 1px #ccc solid;
 }
 
 span {
-   padding: 20px 0 20px 7px;
+  padding: 20px 0 20px 7px;
 }
 
 .search {
-   width: 88px;
-   height: 60px;
+  width: 88px;
+  height: 60px;
 }
 
 .dropdown {
-   margin-top: -67px;
+  margin-top: -67px;
 }
 
 .custom-select {
@@ -115,20 +124,20 @@ span {
 }
 
 .custom-select .selected {
-   padding: 0 10px;
-   height: 60px;
-   margin: 5px 5px 0 5px;
-   font-size: 18px;
-   border: 1px #ccc solid;
-   border-radius: 3px;
-   color: #263a66;
-   outline-color: #00e7eb;
-   cursor: pointer;
-   user-select: none;
+  padding: 0 10px;
+  height: 60px;
+  margin: 5px 5px 0 5px;
+  font-size: 18px;
+  border: 1px #ccc solid;
+  border-radius: 3px;
+  color: #263a66;
+  outline-color: #00e7eb;
+  cursor: pointer;
+  user-select: none;
 }
 
 .custom-select .selected.open {
-  border: 1px solid #00e7eb
+  border: 1px solid #00e7eb;
 }
 
 .custom-select .selected:after {
@@ -148,7 +157,7 @@ span {
   height: 400px;
   border: 1px solid #999;
   position: absolute;
-  background-color: white;
+  background-color: black;
   left: 5px;
   right: 5px;
   z-index: 1;
@@ -161,22 +170,22 @@ span {
   user-select: none;
 }
 
-.custom-select .items div:hover, .custom-select .items div:focus {
+.custom-select .items div:hover,
+.custom-select .items div:focus {
   background-color: #00c7cb;
   color: white;
   outline: none;
 }
 
 .selected:focus {
-   border: 1px solid #00c7cb;
+  border: 1px solid #00c7cb;
 }
 
 .selected:hover {
-   border: 1px black solid;
+  border: 1px black solid;
 }
 
 .selectHide {
   visibility: hidden;
 }
 </style>
-
